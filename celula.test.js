4ba361d5f599782615ecfc46792ea0f211358ecc
Antonio Quijano <antonio.quijanoberne@gmail.com>
2024-03-11 08:59:54 +0100

    Add definitive changes

---
+++ celula.test.js
@@ -0,0 +1,30 @@
+import { game, aliveCells } from './celula.js';
+
+describe('aliveCells', () => {
+  test('should count alive cells correctly', () => {
+    // Arrange
+    const x = [
+      [1, 0, 1],
+      [0, 1, 1],
+      [0, 0, 1],
+    ];
+    const expected = 4;
+    // Act
+    const r = liveCells(x, 1, 1);
+    // Assert
+    expect(r).toBe(expected);
+  });
+  test('should count alive cells correctly', () => {
+    // Arrange
+    const x = [
+      [0, 0, 0],
+      [0, 1, 0],
+      [0, 0, 0],
+    ];
+    const expected = 0;
+    // Act
+    const r = liveCells(x, 1, 1);
+    // Assert
+    expect(r).toBe(expected);
+  });
+});
